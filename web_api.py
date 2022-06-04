c7b74dc900b1754a5791f357a1c8bf0c3fe3d3f4
Paras <punaterparas@gmail.com>
2022-06-04 02:47:57 -0400

    API now fully operational but still need to create documentation and take security measures prior to deployment

--- web_api.py
+++ web_api.py
@@ -10,24 +10,68 @@ ENDPOINT = 'https://data.mongodb-api.com/app/data-kgmzc/endpoint/data/v1'
 API_KEY = 'Oqpbmxqw09Zv5GTxV1K2TUJfOhD3e1OxCi4aOzoioMbThP48Cn1OixovopqAZVHr'
 
 
-def db_insert(payload: dict):
+def serialize_game(game: PyWordle):
+    return pickle.dumps(game, 0).decode()
+
+
+def deserialize_game(game: str):
+    return pickle.loads(game.encode())
+
+
+def db_game_insert(game_id: str, game_obj: PyWordle):
     headers = {'Content-Type': 'application/json', 'Access-Control-Request-Headers': '*', 'api-key': API_KEY}
     payload = {
-          "dataSource": "My-Cluster",
-          "database": "PyWordle-API-Main",
-          "collection": "storage",
-          "document": {
-            payload
-          }
+        "dataSource": "My-Cluster",
+        "database": "PyWordle-API-Main",
+        "collection": "storage",
+        "document": {
+            'game_id': game_id,
+            'game_obj': serialize_game(game_obj)
+        }
     }
     r = requests.post(ENDPOINT + '/action/insertOne', headers=headers, data=json.dumps(payload))
     return r.text
 
-def db_read(game_id: str) -> dict:
-    headers = {'Content-Type': 'application/json', 'Access-Control-Request-Headers': '*', 'api-key': API_KEY}
-    payload = {"dataSource": "My-Cluster", "database": "PyWordle-API-Main", "collection": "storage",
-               "filter": {"game_id": game_id}}
-    r = requests.post(ENDPOINT + '/action/findOne', headers = headers, data = json.dumps(payload))
+
+def db_game_read(game_id: str) -> str:
+    headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': API_KEY
+    }
+    payload = {
+        "dataSource": "My-Cluster",
+        "database": "PyWordle-API-Main",
+        "collection": "storage",
+        "filter":
+            {
+                "game_id": game_id
+            }
+    }
+    r = requests.post(ENDPOINT + '/action/findOne', headers=headers, data=json.dumps(payload))
+    return r.json()['document']['game_obj']
+
+
+def db_game_update(game_id: str, updated_game: PyWordle):
+    headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': API_KEY
+    }
+    payload = {
+        "dataSource": "My-Cluster",
+        "database": "PyWordle-API-Main",
+        "collection": "storage",
+        "filter": {
+                   "game_id": game_id
+               },
+        "update":
+            {
+            'game_id': game_id,
+            'game_obj': serialize_game(updated_game)
+            }
+    }
+    r = requests.post(ENDPOINT + '/action/updateOne', headers=headers, data=json.dumps(payload))
     return r.json()
 
 
@@ -36,23 +80,44 @@ app = Flask(__name__)
 
 @app.route('/create_game', methods=['POST'])
 def create_game():
-    game_id = random.randint(15, 64)
+    game_id = str(random.randint(15, 64))
     new_game = PyWordle()
-    new_game_pickled = pickle.dumps(new_game, 0).decode()
-    db_insert({'game_id': game_id, 'game_obj': new_game_pickled})
+    db_game_insert(game_id = game_id, game_obj = new_game)
     return flask.jsonify({'game_id': game_id}, 201)
+
 
 @app.route('/make_guess', methods=['POST'])
 def make_guess():
     try:
-        game_id = request.form['game_id']
-        guess = request.form['guess']
+        game_id = request.get_json(force = True)['game_id']
+        guess = request.get_json(force = True)['guess']
 
-        game = db_read(game_id)['document']['game_obj']
-        game = pickle.loads(game.encode())
+        game = db_game_read(game_id)
+        game = deserialize_game(game)
+        game_response = game.make_guess(guess)
 
-        game.make_guess(guess)
+        db_game_update(game_id, game)
+
+        return flask.jsonify({'game_response': game_response}, 200)
 
     except:
         return flask.jsonify(400)
+
+
+@app.route('/game_info', methods = ['POST'])
+def game_info():
+    try:
+        game_id = request.get_json(force = True)['game_id']
+        game = db_game_read(game_id)
+        game = deserialize_game(game)
+        game_info_response = game.get_info()
+        print(game_info_response)
+        print(flask.jsonify({'game_info':game_info_response}))
+        return flask.jsonify({'game_info': game_info_response}, 200)
+
+    except:
+        return flask.jsonify(400)
+
+if __name__ == '__main__':
+    app.run()
 
